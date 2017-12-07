c94ba56f5e411520013e9fa2d6aef8f1ad0fdd07
Marcus Davy <marcus.davy@plantandfood.co.nz>
2017-12-08 11:26:59 +1300

    graphics changes, minor bug fix, library sizes

--- GRLaV_Nb_EdgeR.R
+++ GRLaV_Nb_EdgeR.R
@@ -6,6 +6,8 @@ if(!require(edgeR)) {
   library(edgeR)
 }
 
+rm(list=ls())
+
 filename <- '/workspace/hradxj/karmun_awesome_experiment/010.edgeR_Nb/GRLaV3_Nb_EdgeR.tab'
 
 GRLaV3_vs_Nb <- read.delim(filename, header=TRUE)
@@ -62,8 +64,9 @@ panel.points <- function(x, y, col=NULL, loess = FALSE,  maTransform = TRUE, ...
 
 
 panel.corr <- function(x,y, maTransform = TRUE, ...){
-  NAs <- (is.na(x) | is.na(y))
-  correlation <- round(cor(x[!NAs], y[!NAs]), 2)
+  NAs  <- (is.na(x) | is.na(y))
+  infs <- (is.infinite(x) | is.infinite(y))
+  correlation <- round(cor(x[!(NAs | infs)], y[!(NAs | infs)]), 2)
   if(maTransform) {
     text(x=10, y=0, labels=substitute(rho==correlation, list(correlation=correlation)))
   } else {
@@ -72,26 +75,47 @@ panel.corr <- function(x,y, maTransform = TRUE, ...){
 }
 
 ## Healthy.2 replicate is different from the other samples (including the other biological replicate)
-pairs(log2(GRLaV3_vs_Nb+0.5), labels=colnames(tmp), upper.panel=panel.points, lower.panel=panel.corr, 
-      pch=16, cex=0.4, loess=FALSE, maTransform=FALSE)
 
-pairs(log2(GRLaV3_vs_Nb+0.5), labels=colnames(tmp), upper.panel=panel.points, lower.panel=panel.corr, 
-      xlim=c(0,20), ylim=c(-8,8), pch=16, cex=0.4, loess=FALSE)
+par(pty="s")
 
-plotMD.DGEList(de_genes)
+pairs(log2(GRLaV3_vs_Nb), labels=colnames(GRLaV3_vs_Nb), upper.panel=panel.points, lower.panel=panel.corr,
+      pch=16, cex=0.4, loess=FALSE, maTransform=FALSE, asp=1)
+
+pairs(log2(GRLaV3_vs_Nb), labels=colnames(GRLaV3_vs_Nb), upper.panel=panel.points, lower.panel=panel.corr,
+      xlim=c(0,20), ylim=c(-8,8), pch=16, cex=0.4, loess=FALSE, asp=1)
+
 
 plotMDS(log2(GRLaV3_vs_Nb+0.5), top=1000)
+
+
+## How many samples are 0 in All samples?
+# Filtering these has no effect on cpm/rpkm calculations
+apply(GRLaV3_vs_Nb==0, 1, all)  %>%  table
+
 
 # set the replicate relationship.. columns 1 and 2 are reps of each other
 ## Healthy vs Infected
 replicate_relationship <- factor(c(1,1,2))
 # construct the DGEList object
-de_genes <- DGEList(counts=GRLaV3_vs_Nb,group=replicate_relationship)
+all_genes <- DGEList(counts=GRLaV3_vs_Nb, group=replicate_relationship)
+
+for( i in seq(ncol(all_genes))) {
+  plotMD.DGEList(all_genes, column=i)
+  abline(h=0, col="grey", lty=2)
+}
+
+
+breaks <- seq(0, ceiling(max(log2(all_genes$counts))))
+tweak  <- 1
+
+for(i in colnames(all_genes)) {
+  hist(log2(all_genes$counts+tweak)[, i], breaks=breaks, main=i)
+}
 
 # Calculate normalisation factors
-de_genes <- calcNormFactors(de_genes)
+all_genes <- calcNormFactors(all_genes)
 # Examine normalisation factors as a sanity check
-de_genes$samples
+all_genes$samples
 # We can see that Benth.Healthy2 had a lot less sequencing.
 
 # Nonsensical results can happen if we do not remove genes
@@ -99,31 +123,34 @@ de_genes$samples
 # Filter out low expressed genes on the basis of the count per million
 # This filters out genes that are less than cpmlimit, and 
 # that are not expressed in libexplimit number of libraries
-cpmlimit    <- 1000
+cpmlimit    <- 1
 libexplimit <- 3
-keep <- rowSums(cpm(de_genes)>cpmlimit) >= libexplimit
-
-
-tmp <- log2(cpm(de_genes))
-par(pty="s")
-pairs(tmp, pch=16, cex=0.3, asp=1)
+keep <- rowSums(cpm(all_genes)>cpmlimit) >= libexplimit
 
 
 ## Sanity check how many are kept
 table(keep)
 
 
-de_genes_kept <- de_genes[keep, , keep.lib.sizes=FALSE]
+all_genes_kept <- all_genes[keep, , keep.lib.sizes=FALSE]
 # Calculate normalisation factors again
-de_genes_kept <- calcNormFactors(de_genes)
+all_genes_kept <- calcNormFactors(all_genes)
+
+## Sanity check - Benth.Healthy.2 is an order of magnitude smaller!
+calcNormFactors(all_genes)
+apply(GRLaV3_vs_Nb,2, sum)
+
+# Need to look at underlying bam mapping scores...
+
+
 # Examine normalisation factors as a sanity check again
-de_genes_kept$samples
+all_genes_kept$samples
 
 
 # set experiment design
 design <- model.matrix(~replicate_relationship)
 # estimate dispersion
-de_genes_kept <- estimateDisp(de_genes_kept, design)
+all_genes_kept <- estimateDisp(all_genes_kept, design)
 
 
 
@@ -139,7 +166,7 @@ de_genes_kept <- estimateDisp(de_genes_kept, design)
 # no replicates."
 
 # Use the glm to obtain DE genes
-fitglm <- glmFit(de_genes_kept, design)
+fitglm <- glmFit(all_genes_kept, design)
 lrtglm <- glmLRT(fitglm, coef=2)
 topTags(lrtglm, n=10)
 
@@ -149,6 +176,6 @@ topTags(lrtglm, n=10)
 #"While the likelihood ratio test is a more obvious choice for inferences with GLMs, the QL
 #F-test is preferred as it reflects the uncertainty in estimating the dispersion for each gene. It
 #provides more robust and reliable error rate control when the number of replicates is small"
-fitqlm <- glmQLFit(de_genes_kept, design)
+fitqlm <- glmQLFit(all_genes_kept, design)
 qlftest <- glmQLFTest(fitqlm, coef=2)
 topTags(qlftest, n=10)
